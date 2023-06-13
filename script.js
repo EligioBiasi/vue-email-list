31aad00adccceb088e33dfc219f2ed7cf14cb2ad
Eligio Biasi <ex_98@live.it>
2023-06-13 16:01:16 +0200

    list added

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      
+      emailContainer:[],
     }
+  },
+
+  created(){
+    for(let i = 0;i < 10;i++)
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (response) => {
+           this.emailContainer.push(response.data.response)
+        });
   },
 
   methods: {
 
   },
-  
+
 }).mount('#app');
